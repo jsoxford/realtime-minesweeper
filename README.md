73d4b668c01ce30e0c155aed1cb019aabe4fa6f4
peterjwest <peterjwest3@gmail.com>
2015-04-11 15:30:44 +0100

    Fixed static file serving, minified JS

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ Realtime cross browser minesweeper!
 
 Build client side JS with:
 
-    browserify app.js -o bundle.js
+    browserify app.js | uglifyjs > public/bundle.js
+
+You'll need to install browserify and uglify first:
+
+    npm install -g browserify uglify-js
 
 Run server with:
 
--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var path = require('path');
 var game = require('./game');
 
 game.start(16, 16);
-game.getBoard(function(board) {
-  game.watchMoves();
-});
+game.watchMoves();
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
