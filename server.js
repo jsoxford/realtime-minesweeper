87468bdbaa9039e39d765661be6fe7036490491e
peterjwest <peterjwest3@gmail.com>
2015-04-11 15:09:30 +0100

    Fixed file serving

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var game = require('./game');
 
 game.start(16, 16);
@@ -8,6 +9,6 @@ game.getBoard(function(board) {
 
 var app = express();
 app.get('/', function(req, res) {
-    res.sendFile('./index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(3000);
