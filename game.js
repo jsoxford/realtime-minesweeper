e07d69ede41352d0025162e214ce65ac7e1db409
peterjwest <peterjwest3@gmail.com>
2015-04-11 16:44:44 +0100

    Added empty cell move expansion

--- game.js
+++ game.js
@@ -5,14 +5,12 @@ var firebase = new Firebase("https://luminous-inferno-2359.firebaseio.com");
 var game = module.exports = {
   // Resets the game board with random mines
   start: function(width, height) {
-    var self = this;
-
     this.width = width;
     this.height = height;
 
-    this.board = _.times(height, function() {
-      return _.times(width, function() {
-        return { mine: Math.random() < 0.3, open: false, nearby: 0 };
+    this.board = _.times(height, function(y) {
+      return _.times(width, function(x) {
+        return { mine: Math.random() < 0.1, open: false, nearby: 0, x: x, y: y };
       });
     })
 
@@ -23,23 +21,19 @@ var game = module.exports = {
   },
   // Gets the entire board from the server
   getBoard: function(fn) {
-    var self = this;
-
     firebase.child('board').once('value', function(data) {
-      self.board = data.val();
+      game.board = data.val();
       fn(data.val());
     });
   },
   // Watches for moves and applies them to the board
   watchMoves: function() {
-    var self = this;
-
     firebase.child('moves').on('child_added', function(data) {
       data = data.val();
-      if (self.board[data.y][data.x].open === false) {
-        self.board[data.y][data.x].open = data.user;
-        firebase.child('board').set(self.board);
+      if (game.setCell(data.x, data.y, data.user)) {
+        firebase.child('board').set(game.board);
       }
+      game.renderBoard();
     });
   },
   // Watches for changes to the board and sends to a callback
@@ -52,33 +46,41 @@ var game = module.exports = {
   },
   // Makes a move
   makeMove: function(x, y, user) {
-    firebase.child('moves').push({x: x, y: y, user: user});
+    firebase.child('moves').push({ x: x, y: y, user: user });
   },
-  // Calculates the number of mines around a space
-  calculateNearby: function() {
-    var self = this;
+  // Sets a cell to a user
+  setCell: function(x, y, user) {
+    if (this.board[y][x].open !== false) return;
 
+    this.board[y][x].open = user;
+    if (this.board[y][x].nearby === 0) {
+      this.getAdjacents(x, y).map(function(adjacent) {
+        game.setCell(adjacent.x, adjacent.y, user);
+      });
+    }
+    return true;
+  },
+  // Calculates the number of mines around each cell
+  calculateNearby: function() {
     this.board.map(function(row, y) {
-      row.map(function(space, x) {
-        space.nearby = 0;
-        self.getAdjacents(x, y).map(function(sibling) {
-          if (sibling.mine) space.nearby++;
+      row.map(function(cell, x) {
+        cell.nearby = 0;
+        game.getAdjacents(x, y).map(function(adjacent) {
+          if (adjacent.mine) cell.nearby++;
         })
       })
     });
   },
-  // Gets the adjacent spaces for a space
+  // Gets the adjacent cells for a cell, including itself
   getAdjacents: function(x, y) {
-    var self = this;
-
-    var siblings = [];
+    var adjacents = [];
     [-1, 0, 1].map(function(yy) {
       [-1, 0, 1].map(function(xx) {
-        if (self.board[y + yy] && self.board[y + yy][x + xx]) {
-          siblings.push(self.board[y + yy][x + xx]);
+        if (game.board[y + yy] && game.board[y + yy][x + xx]) {
+          adjacents.push(game.board[y + yy][x + xx]);
         }
       });
     });
-    return siblings;
+    return adjacents;
   }
 };
